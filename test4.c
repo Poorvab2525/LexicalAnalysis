4af406f21f838f41063f41a632e4e4e3621a83c2
Bhagwat Poorva Milind <127173860+Poorvab2525@users.noreply.github.com>
2024-08-19 00:28:14 +0530

    Add files via upload

---
+++ test4.c
@@ -0,0 +1,13 @@
+// int main() {
+//     int x = 10;
+//     char *str = "Hello, World!";
+//     printf("%s\n", str);
+//     return 0;
+// }
+
+int main() {
+    /* This is a comment */
+    int x = 10;
+    float y = 20.5;
+    char *str = "Hello, world!";
+}
